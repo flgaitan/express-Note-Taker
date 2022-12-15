13a779dadf124e0c5bd929e01df0dfd839fc384e
flgaitan <fiama_gaitan92@hotmail.com>
2022-12-14 23:07:44 -0600

    fixing folders

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-//const path = require('path');
-const apiRoutes = require('./Develop/routes/apiRoutes');
-const htmlRoutes = require('./Develop/routes/htmlRoutes');
+const path = require('path');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
