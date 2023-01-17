86223c5c07c946ef77c3a946ba43530864aa57a3
flgaitan <fiama_gaitan92@hotmail.com>
2023-01-16 18:05:48 -0600

    deleted lib folder because it is no longer needed, commented out routes, everything is on server.js

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,29 +1,29 @@
-const router = require('express').Router();
-const uniqUid = require('../helpers/uuid');
-const notesDb = require("../db/db.json");
-const {createDb, outputDb} = require("../lib/notes");
+// const router = require('express').Router();
+// const uniqUid = require('../helpers/uuid');
+// const notesDb = require("../db/db.json");
+// const {createDb, outputDb} = require("../lib/notes");
 
-//
-router.get("/notes",(req, res) => {
-    //res.status(200).send(db);
-    let output = notesDb;
-    res.json(output);
- });
+// //
+// router.get("/notes",(req, res) => {
+//     //res.status(200).send(db);
+//     let output = notesDb;
+//     res.json(output);
+//  });
 
 
-router.post("/notes",(req,res) => {
-   req.body.id = uniqUid;
-   const newDb = createDb(req.body, notesDb);
-   res.json(newDb);
+// router.post("/notes",(req,res) => {
+//    req.body.id = uniqUid;
+//    const newDb = createDb(req.body, notesDb);
+//    res.json(newDb);
 
-});
+// });
 
-//BONUS *****
-router.delete("/notes/:id", (req,res) => {
-   const getId = req.params.id;
-   outputDb (getId, notesDb);
-   res.redirect('');
+// //BONUS *****
+// router.delete("/notes/:id", (req,res) => {
+//    const getId = req.params.id;
+//    outputDb (getId, notesDb);
+//    res.redirect('');
  
-});
+// });
 
-module.exports = router;
+// module.exports = router;
--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,37 +1,37 @@
 
-const router = require('express').Router();
+// const router = require('express').Router();
 
 
   
-      // GET Route for feedback page
-  router.get('/notes', (req, res) => {
-    //console.info(`${req.method} request received for notes`);
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
-  });
+//       // GET Route for feedback page
+//   router.get('/notes', (req, res) => {
+//     //console.info(`${req.method} request received for notes`);
+//     res.sendFile(path.join(__dirname, '../public/notes.html'));
+//   });
   
-  router.get('*', (req, res) => {
-  //   console.info(`${req.method} request received for notes`);
-  });
+//   router.get('*', (req, res) => {
+//   //   console.info(`${req.method} request received for notes`);
+//   });
 
-router.get("/", (req, res) => {
-    //res.status(200).send(db);
-    let output = db;
-    res.json(output);
- });
+// router.get("/", (req, res) => {
+//     //res.status(200).send(db);
+//     let output = db;
+//     res.json(output);
+//  });
 
- router.get("/notes", (req, res) => {
-    res.status(200).send(db);
-    let output = db;
-    res.json(output);
- });
-
- 
- router.get("*", (req, res) => {
-    res.status(200).send(db);
-    let output = db;
-    res.json(output);
- });
-
+//  router.get("/notes", (req, res) => {
+//     res.status(200).send(db);
+//     let output = db;
+//     res.json(output);
+//  });
 
  
-module.exports = router;
+//  router.get("*", (req, res) => {
+//     res.status(200).send(db);
+//     let output = db;
+//     res.json(output);
+//  });
+
+
+ 
+// module.exports = router;
